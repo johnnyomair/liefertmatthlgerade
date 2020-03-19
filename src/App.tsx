e7a3e0e8fa187e6e85a67de5a992addfc956739e
Johannes Obermair <johannes.obermair@gmail.com>
2020-03-19 20:04:55 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 import React, { FunctionComponent } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Admin } from "./pages/Admin";
+import { Home } from "./pages/Home";
 
-export const App: FunctionComponent = () => <div>Hello world</div>;
+export const App: FunctionComponent = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/admin" component={Admin} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </BrowserRouter>
+);

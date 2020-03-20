614ba20f571209ce1a5b2a774b89d64274f1f8b7
Johannes Obermair <johannes.obermair@gmail.com>
2020-03-20 19:06:40 +0100

    Add delete ride button

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,26 +1,46 @@
 import { format } from "date-fns";
+import { firestore } from "firebase";
 import React, { FunctionComponent } from "react";
 import { Form } from "../components/Form";
 import { List } from "../components/List";
 import { ListItem } from "../components/ListItem";
-import { useRides } from "../hooks/useRides";
+import { Ride, useRides } from "../hooks/useRides";
+import Styles from "./Admin.module.css";
 
 export const Admin: FunctionComponent = () => {
   const rides = useRides();
 
+  const handleDeleteClick = (ride: Ride) => {
+    firestore()
+      .doc(`rides/${ride.id}`)
+      .delete();
+  };
+
   return (
     <div>
       <h1>Admin</h1>
+      <h2>Neue Fahrt</h2>
       <Form />
       {rides && (
-        <List>
-          {rides.map(ride => (
-            <ListItem key={ride.id}>
-              {format(ride.start, "dd.MM.yy HH:mm")}-{format(ride.end, "HH:mm")}
-            </ListItem>
-          ))}
-          {rides.length === 0 && <ListItem>Keine Fahrten</ListItem>}
-        </List>
+        <>
+          <h2>Fahrten</h2>
+          <List>
+            {rides.map(ride => (
+              <ListItem key={ride.id}>
+                <div className={Styles.listItem}>
+                  <span>
+                    {format(ride.start, "dd.MM.yy, HH:mm")}-
+                    {format(ride.end, "HH:mm")}
+                  </span>
+                  <button onClick={() => handleDeleteClick(ride)}>
+                    Löschen
+                  </button>
+                </div>
+              </ListItem>
+            ))}
+            {rides.length === 0 && <ListItem>Keine Fahrten</ListItem>}
+          </List>
+        </>
       )}
     </div>
   );

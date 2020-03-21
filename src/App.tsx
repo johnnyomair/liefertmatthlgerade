8764d625a44fb810053bde84962ae47fc8a6acf2
Johannes Obermair <johannes.obermair@gmail.com>
2020-03-21 11:32:59 +0100

    Change lazy-loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Text } from "./components/Text";
 import Home from "./pages/Home";
 
-const AdminComponent = lazy(() => import("./pages/Admin"));
-
-const Admin: FunctionComponent = () => (
-  <Suspense fallback={<Text>Lade...</Text>}>
-    <AdminComponent />
-  </Suspense>
-);
+const Admin = lazy(() => import("./pages/Admin"));
 
 export const App: FunctionComponent = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/" component={Home} />
-    </Switch>
-  </BrowserRouter>
+  <Suspense fallback={<Text>Lade...</Text>}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/admin" component={Admin} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  </Suspense>
 );

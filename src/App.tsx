bee39f8b4652aa54a1da067156e28c0abc23cc4b
Johannes Obermair <johannes.obermair@gmail.com>
2020-03-21 11:17:59 +0100

    Add lazy-loading for Admin page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Admin from "./pages/Admin";
+import { Text } from "./components/Text";
 import Home from "./pages/Home";
+
+const AdminComponent = lazy(() => import("./pages/Admin"));
+
+const Admin: FunctionComponent = () => (
+  <Suspense fallback={<Text>Lade...</Text>}>
+    <AdminComponent />
+  </Suspense>
+);
 
 export const App: FunctionComponent = () => (
   <BrowserRouter>

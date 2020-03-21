120ab4e8c155894640f2d96b6e7f8b76cf8660d2
Johannes Obermair <johannes.obermair@gmail.com>
2020-03-21 09:55:40 +0100

    Add validation to form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,40 +1,115 @@
-import { Interval, parse } from "date-fns";
+import { Interval, parse, isPast, endOfDay } from "date-fns";
+import { FormApi, ValidationErrors } from "final-form";
 import { firestore } from "firebase/app";
-import React, { FormEvent, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
+import { Field, Form as FinalForm } from "react-final-form";
+import { Input } from "./Input";
+import Styles from "./Form.module.css";
+
+interface FormValues {
+  date?: string;
+  start?: string;
+  end?: string;
+}
+
+const validate = (values: FormValues): ValidationErrors => {
+  const { date, start, end } = values;
+
+  const errors: ValidationErrors = {};
+
+  if (date) {
+    const parsedDate = parse(date, "yyyy-MM-dd", new Date());
+
+    if (isPast(endOfDay(parsedDate))) {
+      errors.date = "Datum darf nicht in der Vergangenheit sein";
+    }
+  } else {
+    errors.date = "Pflichtfeld";
+  }
+
+  if (start) {
+    if (end) {
+      const parsedStart = parse(start, "HH:mm", new Date());
+      const parsedEnd = parse(end, "HH:mm", new Date());
+
+      if (parsedStart.getTime() > parsedEnd.getTime()) {
+        errors.start = "Start darf nicht nach Ende sein";
+      }
+    }
+  } else {
+    errors.start = "Pflichtfeld";
+  }
+
+  if (end) {
+    if (start) {
+      const parsedStart = parse(start, "HH:mm", new Date());
+      const parsedEnd = parse(end, "HH:mm", new Date());
+
+      if (parsedStart.getTime() > parsedEnd.getTime()) {
+        errors.end = "Ende darf nicht vor Start sein";
+      }
+    }
+  } else {
+    errors.end = "Pflichtfeld";
+  }
+
+  return errors;
+};
 
 export const Form: FunctionComponent = () => {
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const date = event.currentTarget.date.value;
-    const start = event.currentTarget.start.value;
-    const end = event.currentTarget.end.value;
+  const handleSubmit = (values: FormValues, form: FormApi<FormValues>) => {
+    const { date, start, end } = values;
 
     const newRide: Interval = {
       start: parse(`${date} ${start}`, "yyyy-MM-dd HH:mm", new Date()),
       end: parse(`${date} ${end}`, "yyyy-MM-dd HH:mm", new Date())
     };
 
-    firestore()
+    return firestore()
       .collection("rides")
       .add(newRide);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="date">Datum</label>
-        <input id="date" name="date" type="date" required />
-      </div>
-      <div>
-        <label htmlFor="start">Start</label>
-        <input id="start" name="start" type="time" required />
-      </div>
-      <div>
-        <label htmlFor="end">Ende</label>
-        <input id="end" name="end" type="time" required />
-      </div>
-      <button>Speichern</button>
-    </form>
+    <FinalForm
+      onSubmit={handleSubmit}
+      validate={validate}
+      render={({ handleSubmit, invalid, pristine, form }) => (
+        <form
+          onSubmit={async event => {
+            await handleSubmit(event);
+            form.reset();
+          }}
+        >
+          <Field
+            id="date"
+            name="date"
+            type="date"
+            label="Datum"
+            component={Input}
+            required
+          />
+          <Field
+            id="start"
+            name="start"
+            type="time"
+            label="Start"
+            component={Input}
+            required
+          />
+          <Field
+            id="end"
+            name="end"
+            type="time"
+            label="End"
+            component={Input}
+            required
+          />
+          <div className={Styles.buttonContainer}>
+            <button disabled={invalid || pristine}>Speichern</button>
+          </div>
+        </form>
+      )}
+    />
   );
 };
---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+import { FieldRenderProps } from "react-final-form";
+import Styles from "./Input.module.css";
+
+export const Input: FunctionComponent<FieldRenderProps<any, HTMLElement> & {
+  label: string;
+}> = ({ input, meta, label }) => (
+  <>
+    <div className={Styles.inputContainer}>
+      <label className={Styles.label}>{label}</label>
+      <input className={Styles.input} {...input} />
+    </div>
+    {meta.dirty && meta.error && (
+      <span className={Styles.error}>{meta.error}</span>
+    )}
+  </>
+);
